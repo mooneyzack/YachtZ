07d0eed8d0338952437a1237347701e6241a6da7
mooneyzack <z.t.mooney@outlook.com>
2024-05-18 23:54:07 -0500

    - index.html: added section totals - script.js: added value property to dice object, with setter & getter - script.js: created Dice array 'allDice' for iteration during score calculation - script.js: modified rollFreeDice() to set 'value' for each un-held dice, and to then display the value - script.js: created function calcScore() to account for states of all dice in order to determine scoring - script.js: modified saveScore() to call calcScore based on type of target

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 //Click roll button
+
+
 class Dice {
   holdState;
+  value;
+
   constructor() {
     this.holdState = false;
     this.value = null;
   }
+  setValue(newValue) {
+    this.value = newValue;
+  }
+
+  getValue() {
+    return this.value;
+  }
 }
+
+
+
+// Optional class for rolling
+// class Roll {
+//   rollState;
+//
+//   constructor() {
+//     this.rollState = 0;
+//   }
+// }
 
 let Dice1 = new Dice();
 let Dice2 = new Dice();
@@ -13,13 +35,13 @@ let Dice3 = new Dice();
 let Dice4 = new Dice();
 let Dice5 = new Dice();
 
+let allDice = [Dice1, Dice2, Dice3, Dice4, Dice5];
+
 document.addEventListener("DOMContentLoaded", initEventListeners);
 
 function initEventListeners() {
-
   // roll free dice
   document.querySelector('#roll').addEventListener("click", rollFreeDice);
-
   // functions to hold individual dice
   document.querySelector('#dice1').addEventListener("click", function() {holdDice(Dice1)});
   document.querySelector('#dice2').addEventListener("click", function() {holdDice(Dice2)});
@@ -44,20 +66,39 @@ function initEventListeners() {
 
 //Randomize dice value 1-6
 function rollFreeDice() {
-  if (!Dice1.holdState) {document.querySelector('#dice1').textContent = Math.floor(Math.random() * 6) + 1;}
-  if (!Dice2.holdState) {document.querySelector('#dice2').textContent = Math.floor(Math.random() * 6) + 1;}
-  if (!Dice3.holdState) {document.querySelector('#dice3').textContent = Math.floor(Math.random() * 6) + 1;}
-  if (!Dice4.holdState) {document.querySelector('#dice4').textContent = Math.floor(Math.random() * 6) + 1;}
-  if (!Dice5.holdState) {document.querySelector('#dice5').textContent = Math.floor(Math.random() * 6) + 1;}
+  if (!Dice1.holdState) {
+    Dice1.setValue(Math.floor(Math.random() * 6) + 1);
+    document.querySelector('#dice1').textContent = Dice1.getValue();
+    //document.querySelector('#dice1').textContent = Math.floor(Math.random() * 6) + 1;
+  }
+  if (!Dice2.holdState) {
+    Dice2.setValue(Math.floor(Math.random() * 6) + 1);
+    document.querySelector('#dice2').textContent = Dice2.getValue();
+    //document.querySelector('#dice2').textContent = Math.floor(Math.random() * 6) + 1;
+  }
+  if (!Dice3.holdState) {
+    Dice3.setValue(Math.floor(Math.random() * 6) + 1);
+    document.querySelector('#dice3').textContent = Dice3.getValue();
+    //document.querySelector('#dice3').textContent = Math.floor(Math.random() * 6) + 1;
+  }
+  if (!Dice4.holdState) {
+    Dice4.setValue(Math.floor(Math.random() * 6) + 1);
+    document.querySelector('#dice4').textContent = Dice4.getValue();
+    //document.querySelector('#dice4').textContent = Math.floor(Math.random() * 6) + 1;
+  }
+  if (!Dice5.holdState) {
+    Dice5.setValue(Math.floor(Math.random() * 6) + 1);
+    document.querySelector('#dice5').textContent = Dice5.getValue();
+    //document.querySelector('#dice5').textContent = Math.floor(Math.random() * 6) + 1;
+  }
 }
 
-
-
 //Add event listener to each individual dice that calls a hold function
-
 function holdDice(dice) {
   dice.holdState = !dice.holdState;
-  if (dice.holdState) {event.target.classList.add("held")}
+  if (dice.holdState) {
+    event.target.classList.add("held")
+  }
   else {event.target.classList.remove("held")}
 }
 
@@ -69,6 +110,127 @@ function holdDice(dice) {
 //---stay simple - make it work badly, and then improve
 //---get a score from the dice to be saved into the box
 
+// function calcScoreSingle(numVal) {
+//   let counter = 0;
+//   for (let dice of allDice) {
+//     if (dice.getValue() === numVal) {
+//       counter++;
+//     }
+//   }
+//   return counter * numVal;
+// }
+
+function calcScore(scoreNum) {
+  let counter1 = 0;
+  let counter2 = 0;
+  let counter3 = 0;
+  let counter4 = 0;
+  let counter5 = 0;
+  let counter6 = 0;
+
+  let totVal = 0;
+  for (let dice of allDice) {
+    if (dice.getValue() === 1) {counter1++;}
+    if (dice.getValue() === 2) {counter2++;}
+    if (dice.getValue() === 3) {counter3++;}
+    if (dice.getValue() === 4) {counter4++;}
+    if (dice.getValue() === 5) {counter5++;}
+    if (dice.getValue() === 6) {counter6++;}
+    totVal = totVal + dice.getValue();
+  }
+
+  //top section/individual numbers
+  if (scoreNum === 1) {return counter1;}
+  if (scoreNum === 2) {return counter2 * 2;}
+  if (scoreNum === 3) {return counter3 * 3;}
+  if (scoreNum === 4) {return counter4 * 4;}
+  if (scoreNum === 5) {return counter5 * 5;}
+  if (scoreNum === 6) {return counter6 * 6;}
+
+  //three of a kind
+  if (scoreNum === 7) {
+    if (counter1 >= 3 ||
+        counter2 >= 3 ||
+        counter3 >= 3 ||
+        counter4 >= 3 ||
+        counter5 >= 3 ||
+        counter6 >= 3) {
+      return totVal;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  //four of a kind
+  if (scoreNum === 8) {
+    if (counter1 >= 4 ||
+        counter2 >= 4 ||
+        counter3 >= 4 ||
+        counter4 >= 4 ||
+        counter5 >= 4 ||
+        counter6 >= 4) {
+      return totVal;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  //full house
+  if (scoreNum === 9) {
+    return 25;
+  }
+
+  //small straight
+  if (scoreNum === 10) {
+    return 30;
+  }
+
+  //large straight
+  if (scoreNum === 11) {
+    if (
+        (counter2 === 1 && counter3 === 1 && counter4 === 1 && counter5 === 1)
+        &&
+        (counter1 === 1 || counter6 === 1)
+    )
+    {
+      return 40;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  //yahtzee
+  if (scoreNum === 12) {
+    if (counter1 === 5 ||
+        counter2 === 5 ||
+        counter3 === 5 ||
+        counter4 === 5 ||
+        counter5 === 5 ||
+        counter6 === 5) {
+      return 50;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  //second yahtzee
+  if (scoreNum === 13) {
+    return 50;
+  }
+
+  //chance
+  if (scoreNum === 14) {
+    return totVal;
+  }
+}
+
+
+
+
 
 
 //Start adding logic for calculating score accurately
@@ -76,10 +238,22 @@ function holdDice(dice) {
 function saveScore() {
   console.log(event.target.parentNode.id);
   type = event.target.parentNode.id;
-  if (type == "ones") {
-    console.log("Helloooo");
-  }
-  event.target.textContent = "placeholder";
+
+  if (type == "ones") {event.target.textContent = calcScore(1);}
+  if (type == "twos") {event.target.textContent = calcScore(2);}
+  if (type == "threes") {event.target.textContent = calcScore(3);}
+  if (type == "fours") {event.target.textContent = calcScore(4);}
+  if (type == "fives") {event.target.textContent = calcScore(5);}
+  if (type == "sixes") {event.target.textContent = calcScore(6);}
+
+  if (type == "three-kind") {event.target.textContent = calcScore(7);}
+  if (type == "four-kind") {event.target.textContent = calcScore(8);}
+  if (type == "full-house") {event.target.textContent = calcScore(9);}
+  if (type == "small-straight") {event.target.textContent = calcScore(10);}
+  if (type == "large-straight") {event.target.textContent = calcScore(11);}
+  if (type == "first-yahtzee") {event.target.textContent = calcScore(12);}
+  if (type == "second-yahtzee") {event.target.textContent = calcScore(13);}
+  if (type == "chance") {event.target.textContent = calcScore(14);}
 }
 
 
